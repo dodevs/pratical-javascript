5da5110f387c2c8538fcc662926733e67b8d17bf
dodevs <douglass.sousa@outlook.com.br>
2018-07-03 15:37:28 -0300

    Versao 1 Finalizada

---
+++ version1.js
@@ -0,0 +1,5 @@
+var todos = ['item1','item2','item3'] //Requeriment 1
+console.log('My todos: ', todos ); //Requeriment 2
+todos.push('item4') //Requeriment 3 - retorna o indice do novo item
+todos[0] = 'item1 updated' //Requeriments 4 
+todos.splice(0,1) //Requeriment 5 - deleta um elemento a partir do indice 0
