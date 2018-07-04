070534f396099c6f5f56eb5c22032e2f955354a5
dodevs <douglass.sousa@outlook.com.br>
2018-07-04 14:41:40 -0300

    Versão 3 Finalizada

--- version3.js
+++ version3.js
@@ -2,3 +2,27 @@ var douglas = {
     nome: 'Douglas',
     dizerNome: function(){console.log(this.name)}
 };
+
+//Requeriment 1
+var todos_object = {
+    todos: [],
+    //Requeriment 2
+    displayTodos: function(){
+        console.log(this.todos);
+    },
+    //Requeriment 3
+    addTodo: function(todo) {
+        this.todos.push(todo); 
+        this.displayTodos();
+    }, 
+    //Requeriment 4
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    //Requeriment 5
+    deleteTodo: function(position) {
+        this.todos.splice(position,1);
+        this.displayTodos();
+    }
+};
